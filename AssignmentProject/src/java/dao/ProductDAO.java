b4114ef9e746512538147c2208e71529fd6843c4
chuvanluan2211 <chuvanluan2211@gmail.com>
2022-03-12 23:57:41 +0700

    login va signup

--- AssignmentProject/src/java/dao/ProductDAO.java
+++ AssignmentProject/src/java/dao/ProductDAO.java
@@ -5,6 +5,7 @@
  */
 package dao;
 
+import entity.Login;
 import entity.Product;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,6 +18,7 @@ import java.util.List;
  * @author laptop 2019
  */
 public class ProductDAO {
+
     public List<Product> getAllProduct() {
         List<Product> list = new ArrayList<>();
         String query = "select * from product \n";
@@ -36,7 +38,7 @@ public class ProductDAO {
                         rs.getDouble(4),
                         rs.getString(5),
                         rs.getString(6));
-                
+
                 list.add(p);
             }
 
@@ -46,13 +48,14 @@ public class ProductDAO {
         return list;
 
     }
-    public Product getLast(){
-        String query = "select top 1 * from product\n" +
-                  "order by id desc";
+
+    public Product getLast() {
+        String query = "select top 1 * from product\n"
+                + "order by id desc";
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-         try {
+        try {
             conn = Model.getConnection();
             ps = conn.prepareStatement(query);
             rs = ps.executeQuery();
@@ -63,7 +66,7 @@ public class ProductDAO {
                         rs.getDouble(4),
                         rs.getString(5),
                         rs.getString(6));
-                
+
                 return p;
             }
 
@@ -73,6 +76,151 @@ public class ProductDAO {
         return null;
     }
 
+    public List<Product> searchByName(String txtSearch) {
+        List<Product> list = new ArrayList<>();
+        String query = "select  * from product\n"
+                + "where [name] like ?";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = Model.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, "%" + txtSearch + "%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Product p = new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+
+                list.add(p);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+
+    }
+
+    public Product getById(String id) {
+        String query = "select top 1 * from product\n"
+                + "where id = ?";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = Model.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Product p = new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+
+                return p;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public Login getAccount(String user, String pass) {
+        String query = "select * from Account\n"
+                + "where [user] = ?\n"
+                + "and [pass] = ?";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = Model.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Login a = new Login(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5)
+                );
+                return a;
+
+            }
+
+        } catch (Exception e) {
+
+        }
+        return null;
+
+    }
+    public Login checkAccount(String user) {
+        String query = "select * from Account\n"
+                + "where [user] = ?\n"
+               ;
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = Model.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Login a = new Login(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5)
+                );
+                return a;
+
+            }
+
+        } catch (Exception e) {
+
+        }
+        return null;
+
+    }
+    public void signUp(String user, String pass){
+        String query = "insert into Account\n"
+                + "values (?,?,0,0)\n"
+               ;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = Model.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            ps.executeUpdate();
+            
+
+        } catch (Exception e) {
+
+        }
+    }
     public static void main(String[] args) {
         ProductDAO db = new ProductDAO();
         List<Product> list = new ArrayList<>();
@@ -80,5 +228,5 @@ public class ProductDAO {
         for (Product o : list) {
             System.out.println(o);
         }
-    } 
+    }
 }
--- AssignmentProject/web/Detail.jsp
+++ AssignmentProject/web/Detail.jsp
@@ -11,6 +11,10 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 		<title>TransformerWorld Toy</title>
@@ -40,10 +44,42 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+        <style>
+            .gallery-wrap .img-big-wrap img {
+                height: 450px;
+                width: auto;
+                display: inline-block;
+                cursor: zoom-in;
+            }
 
+
+            .gallery-wrap .img-small-wrap .item-gallery {
+                width: 60px;
+                height: 60px;
+                border: 1px solid #ddd;
+                margin: 7px 2px;
+                display: inline-block;
+                overflow: hidden;
+            }
+
+            .gallery-wrap .img-small-wrap {
+                text-align: center;
+            }
+            .gallery-wrap .img-small-wrap img {
+                max-width: 100%;
+                max-height: 100%;
+                object-fit: cover;
+                border-radius: 4px;
+                cursor: zoom-in;
+            }
+            .img-big-wrap img{
+                width: 100% !important;
+                height: auto !important;
+            }
+        </style>
     </head>
 	<body>
-            <form action="Home" method="Get">
+            <form action="Detail" method="Get">
 		<!-- HEADER -->
 		<header>
 			<!-- TOP HEADER -->
@@ -165,6 +201,75 @@
 			<!-- /MAIN HEADER -->
 		</header>
 		<!-- /HEADER -->
+
+
+        <div class="container">
+            <div class="row">
+            <jsp:include page="Left.jsp"></jsp:include>
+            <div class="col-sm-9">
+                <div class="container">
+                    <div class="card">
+                        <div class="row">
+                            <aside class="col-sm-5 border-right">
+                                <article class="gallery-wrap"> 
+                                    <div class="img-big-wrap">
+                                        <div> <a href="#"><img  src="./images/${detail.image}"></a></div>
+                                    </div> <!-- slider-product.// -->
+                                    <div class="img-small-wrap">
+                                    </div> <!-- slider-nav.// -->
+                                </article> <!-- gallery-wrap .end// -->
+                            </aside>
+                            <aside class="col-sm-7">
+                                <article class="card-body p-5">
+                                    <h3 class="title mb-3">${detail.name}</h3>
+
+                                    <p class="price-detail-wrap"> 
+                                        <span class="price h3 text-warning"> 
+                                            <span class="currency">US $</span><span class="num">${detail.price}</span>
+                                        </span> 
+                                    </p> <!-- price-detail-wrap .// -->
+                                    <dl class="item-property" >
+                                        <dt>Description</dt>
+                                        <dd><p>
+                                                ${detail.description}
+                                            </p></dd>
+                                    </dl>
+
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-sm-5">
+                                            <dl class="param param-inline">
+                                                <dt>Quantity: </dt>
+                                                <dd>
+                                                    <select class="form-control form-control-sm" style="width:70px;">
+                                                        <option> 1 </option>
+                                                        <option> 2 </option>
+                                                        <option> 3 </option>
+                                                    </select>
+                                                </dd>
+                                            </dl>  <!-- item-property .// -->
+                                        </div> <!-- col.// -->
+
+                                    </div> <!-- row.// -->
+                                    <hr>
+                                    <a href="#" class="btn btn-lg btn-primary text-uppercase"> Buy now </a>
+                                    <a href="#" class="btn btn-lg btn-outline-primary text-uppercase"> <i class="fas fa-shopping-cart"></i> Add to cart </a>
+                                </article> <!-- card-body.// -->
+                            </aside> <!-- col.// -->
+                        </div> <!-- row.// -->
+                    </div> <!-- card.// -->
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
 
         		<!-- FOOTER -->
 		<footer id="footer">
